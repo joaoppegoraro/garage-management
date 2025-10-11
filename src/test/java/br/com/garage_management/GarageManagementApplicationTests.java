53c462e7fc42d85720591c4116c81ccfa048e852
Joao Pedro Pegoraro <joaoppegoraro99@gmail.com>
2025-10-11 11:02:09 -0300

    Initial project structure

---
+++ src/test/java/br/com/garage_management/GarageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.garage_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
